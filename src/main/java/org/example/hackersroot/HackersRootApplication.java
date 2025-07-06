f6d5f65a0dff88367806db989c9627e6fc203e30
hyebin <shin378378@naver.com>
2025-07-06 22:26:27 +0900

    first

---
+++ src/main/java/org/example/hackersroot/HackersRootApplication.java
@@ -0,0 +1,13 @@
+package org.example.hackersroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackersRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackersRootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hackersroot/HackersRootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hackersroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackersRootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
